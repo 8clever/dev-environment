e263ef5e623a46168dbb5fe51a8c6e621812042a
Ivan Vityaev <godofluck89@gmail.com>
2025-06-23 18:54:18 +0300

    fix

--- cli/utils/input_select.py
+++ cli/utils/input_select.py
@@ -1,8 +1,8 @@
 def input_select (prompt: str, options: list):
   print(f"{prompt}:")
   for option in options:
-     print(f" - {option}")
+    print(f" - {option}")
   response = None
   while response not in options:
-      response = input("Input: ")
+    response = input("Input: ")
   return response
